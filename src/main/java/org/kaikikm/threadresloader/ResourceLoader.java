cd6e2419ef0b97d190f0b2f31b5e1ac8f4976571
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-30 17:09:32 +0200

    style: remove unnecessary fully qualified access

--- src/main/java/org/kaikikm/threadresloader/ResourceLoader.java
+++ src/main/java/org/kaikikm/threadresloader/ResourceLoader.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
 
+import static java.lang.Class.*;
+
 /**
  * Static class that offers a thread-dependent resource loading.
  * Each thread has independent, configurable, runtime-modifiable classpath for resource loading.
@@ -30,25 +32,25 @@ public final class ResourceLoader {
      * specified by {@link Thread#setContextClassLoader(ClassLoader)}.
      */
     public static void setDefault() {
-        ResourceLoader.CLASS_LOADER.set(new AccessibleURLClassLoader());
+        CLASS_LOADER.set(new AccessibleURLClassLoader());
     }
 
     /**
      * Set current thread resource loader with default thread resource loading settings,
      * specified by {@link Thread#setContextClassLoader(ClassLoader)}.
      * Adds given URLs to default.
-     * 
+     *
      *  @param urls URLs that have to be added
      */
     public static void setURLs(final URL... urls) {
-        ResourceLoader.CLASS_LOADER.set(new AccessibleURLClassLoader(urls));
+        CLASS_LOADER.set(new AccessibleURLClassLoader(urls));
     }
 
     /**
     * Set current thread resource loader with default thread resource loading settings,
      * specified by {@link Thread#setContextClassLoader(ClassLoader)}.
     * Adds given URLs to default.
-    * 
+    *
     *  @param urls URLs that have to be added
     */
     public static void setURLs(final Collection<URL> urls) {
@@ -59,18 +61,18 @@ public final class ResourceLoader {
      * Set current thread resource loader with parent thread resource loading settings
      * (default {@link ResourceLoader} behavior).
      * Adds given URLs to default.
-     * 
+     *
      *  @param urls URLs that have to be added
      */
     public static void injectURLs(final URL... urls) {
-        ResourceLoader.CLASS_LOADER.set(new AccessibleURLClassLoader(CLASS_LOADER.get(), urls));
+        CLASS_LOADER.set(new AccessibleURLClassLoader(CLASS_LOADER.get(), urls));
     }
 
     /**
      * Set current thread resource loader with parent thread resource loading settings
      * (default {@link ResourceLoader} behavior).
      * Adds given URLs to default.
-     * 
+     *
      *  @param urls URLs that have to be added
      */
     public static void injectURLs(final Collection<URL> urls) {
@@ -79,7 +81,7 @@ public final class ResourceLoader {
 
     /**
      * Get resource's URL. If resource loader settings aren't previously specified it uses the parent thread settings.
-     * 
+     *
      * @param path Resource's path
      * @return Resource's URL
      */
@@ -90,7 +92,7 @@ public final class ResourceLoader {
     /**
     * Get resource's {@link InputStream}.
      * If resource loader settings aren't previously specified it uses the parent thread settings.
-    * 
+    *
     * @param path Resource's path
     * @return Resource's URL
     */
@@ -103,24 +105,24 @@ public final class ResourceLoader {
      * @param url the URL to add.
      */
     public static void addURL(final URL url) {
-        ResourceLoader.CLASS_LOADER.get().addURL(url);
+        CLASS_LOADER.get().addURL(url);
     }
 
     /**
-     * 
+     *
      * @return Class Loader that library uses for current thread
      */
     public static ClassLoader getClassLoader() {
-        return ResourceLoader.CLASS_LOADER.get();
+        return CLASS_LOADER.get();
     }
 
     /**
      * Returns the Class object associated with the class or interface with the given string name.
-     * Invoking this method is equivalent to: 
+     * Invoking this method is equivalent to:
      * <blockquote>
      *  {@code ResourceLoader.classForName(className, true)}
      * </blockquote>
-     * 
+     *
      * @param name Fully qualified name of the desired class
      * @return Class object representing the desired class
      * @throws ClassNotFoundException if the class cannot be found.
@@ -133,14 +135,14 @@ public final class ResourceLoader {
      * Returns the Class object associated with the class or interface with the given string name. Classes on
      * local classpath will override classes with same name that are in the system classpath or in the parent thread
      * classpath (reverse behavior compared to standard java {@link ClassLoader})
-     * 
+     *
      * @param initialize If true the class will be initialized
      * @param name Fully qualified name of the desired class
      * @return Class Object representing the desired class
      * @throws ClassNotFoundException if the class cannot be found.
      */
     public static Class<?> classForName(final String name, final boolean initialize) throws ClassNotFoundException {
-        return Class.forName(name, initialize, ResourceLoader.CLASS_LOADER.get());
+        return forName(name, initialize, CLASS_LOADER.get());
     }
 
 }
